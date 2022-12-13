d4eaf7387317b2cf685616ad9daea5c28b3ad559
khairul <khairulbahri281>
2022-12-13 19:52:42 +0700

    perbaikan tipis-tipis

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BiodataPengunjung;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $biodata = BiodataPengunjung::orderByDesc("id")->get();
+
+        return view('biodata-pengunjung.index', compact(['biodata']));
     }
 }
